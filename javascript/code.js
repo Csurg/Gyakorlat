cee57a8147e869ac2d537892a3d03f1fa7553bf6
Csurg <matkovity77@gmail.com>
2025-01-14 13:43:52 +0100

    js

--- javascript/code.js
+++ javascript/code.js
@@ -87,7 +87,7 @@ let continueBtn = document.getElementById("continue-btn");
 
 window.addEventListener("scroll", function() {
   clearTimeout(timeout);
-  timeout = setTimeout(showPopup, 10000);
+  timeout = setTimeout(showPopup, 30000);
 });
 
 function showPopup() {
@@ -104,3 +104,36 @@ function closePopup() {
   localStorage.setItem("popupShown", "true"); // Set flag in localStorage to indicate popup has been shown
 }
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark'); //add this
+  }
+  else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light'); //add this
+  }    
+}
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
